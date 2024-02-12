606177f5253a7fb91c8c36e946a0139ace2e42ca
NABIL234234 <sarzanovnabil108@gmail.com>
2024-02-12 15:04:16 +0600

    add URL

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ mbankBlock.onclick = () => {
     "https://mbank.kg/credits/online-kredit?utm_source=google&utm_medium=cpc&utm_campaign=credits_poisk&utm_content=adapt_online2&utm_term=%D0%BC%20%D0%B1%D0%B0%D0%BD%D0%BA%20%D0%BA%D1%80%D0%B5%D0%B4%D0%B8%D1%82&gclid=CjwKCAiA2pyuBhBKEiwApLaIO7mJnnPX99aMALN22YpdjF5J3kPDrIOMdvPR6tPrEw02UvzO9qXo1BoC1HAQAvD_BwE";
 };
 itBlock.onclick = () => {
-  window.location.href = "";
+  window.location.href = "https://fait.netlify.app";
 };
 careerBlock.onclick = () => {
-  window.location.href = "";
+  window.location.href = "https://facareer.netlify.app";
 };
 chessBlock.onclick = () => {
-  window.location.href = "";
+  window.location.href = "https://fachess.netlify.app";
 };
 
 let isBurgerMenuActive = false;
